ab4f013a294c1e6289ef5521b1b3ae1d1616d9e0
BatteryDon <donatshneps@gmail.com>
2024-07-26 10:55:07 +0300

    Add files via upload

---
+++ start-handler.py
@@ -0,0 +1,11 @@
+import telebot
+
+token='7111958435:AAEismGDGX3LMqPhsnEYJo_q4TtcSC7izME'
+
+bot=telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+
+def start_message(message):
+  bot.send_message(message.chat.id,"Привет, Андрей!")
+bot.infinity_polling()
